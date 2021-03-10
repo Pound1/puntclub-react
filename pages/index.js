7c0a22c8a4ccea31367b171618c576ec61e43e83
Thomas Bye <thomas@rare.com.au>
2021-03-10 11:53:29 +1100

    First Commit, create-next-app

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Punt Club</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>Main</main>
+
+      <footer className={styles.footer}>Footer</footer>
+    </div>
+  );
+}
