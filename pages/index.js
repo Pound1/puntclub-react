84a7c1d6542e0414fbbe5ea21bfd9bdc763f7beb
Thomas Bye <thomas@rare.com.au>
2021-03-12 17:29:12 +1100

    Changed back to functional component

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,27 @@
-import { withSession } from "next-auth/client";
+import { useSession } from "next-auth/client";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <main>
-          <div>
-            {loading && <div>Loading...</div>}
-            {session && (
-              <>
-                <p>Welcome, {this.props.session.user.nickname}</p>
-                <br />
-                <img src={this.props.session.user.image} alt="" />
-              </>
-            )}
-            {!session && (
-              <>
-                <p>Please Sign in</p>
-              </>
-            )}
-          </div>
-        </main>
-      </div>
-    );
-  }
+export default function Home() {
+  const [session, loading] = useSession();
+
+  return (
+    <div>
+      <main>
+        <div>
+          {loading && <div>Loading...</div>}
+          {session && (
+            <>
+              <p>Welcome, {session.user.nickname}</p>
+              <br />
+              <img src={session.user.image} alt="" />
+            </>
+          )}
+          {!session && (
+            <>
+              <p>Please Sign in</p>
+            </>
+          )}
+        </div>
+      </main>
+    </div>
+  );
 }
-export default withSession(Home);
